58ee31e9b4656b29ab8501034e1826e1c1d18e3c
BAbdullaev <abdullaevbilaliddin@gmail.com>
2022-06-17 17:49:23 -0500

    Updated text on Main.js

--- client/src/comp/Main.js
+++ client/src/comp/Main.js
@@ -8,11 +8,11 @@ function Main() {
         <div className="w-[95%] flex justify-around items-center m-auto flex-wrap overflow-hidden">
 
             <div className="flex flex-col items-start justify-between w-[450px] p-4 rounded-xl break-words">
-                <h1 className="text-[44px] font-semibold tracking-[1px] mt-[10px] mb-3 leading-[1.3]">Create a fun free Jeopardy</h1>
+                <h1 className="text-[44px] font-semibold tracking-[1px] mt-[10px] mb-3 leading-[1.3]">Create a fun free game of Jeopardy</h1>
                 <p className="text-[18px] font-semibold mb-5">
-                With our amazing website, you can create flawless, performant, Stylish and fun Jeopardies while also learning and having fun at the same time!
+                With our amazing website, you can create flawless, performant, Stylish and fun Jeopardy games while also learning and having fun at the same time!
                 </p>
-                <Link to="/create" className="w-full m-auto pt-[6px] pb-[6px] font-semibold rounded-xl mb-[10px] bg-[#6b63ffc6] text-white text-center transition-all _shadow">Create a jeopardy</Link>
+                <Link to="/create" className="w-full m-auto pt-[6px] pb-[6px] font-semibold rounded-xl mb-[10px] bg-[#6b63ffc6] text-white text-center transition-all _shadow">Create a jeopardy game</Link>
             </div>
 
             <div className="">
